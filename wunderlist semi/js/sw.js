25ddb41916e24c59553ce669cda575641337ee89
Chazzevns <2165731@slu.edu.ph>
2019-10-28 01:59:32 +0800

    added service worker. Pero di pa sya na configure ng maayos.

---
+++ wunderlist semi/js/sw.js
@@ -0,0 +1,11 @@
+UpUp.start({
+    'cache-version': 'v2',
+    'content-url': 'index.html',
+    'assets': [
+        'style/style.css',
+        'img/back.jpg',
+        'img/logo.png'
+    ],
+    
+    'service-worker-url': 'js/upup.sw.min.js'
+})
